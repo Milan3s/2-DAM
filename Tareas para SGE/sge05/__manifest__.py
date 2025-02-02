571efa9120af0406ce47a6e009788e102422eead
David Milanes Moreno <35001922+Milan3s@users.noreply.github.com>
2025-02-02 20:50:47 +0100

    Add files via upload

---
+++ Tareas para SGE/sge05/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'organizarcitas',
+    'version': '1.0',
+    'category': 'Servicios',
+    'author': 'David Milanés Moreno',
+    'website': 'https://www.tusitio.com',
+    # dependencia base 
+    'depends': ['base'],
+    # datos predefinidos
+    'data': [
+        'views/reportes.xml',
+        'security/groups.xml',
+        'views/cita_vistas.xml',
+        'security/ir.model.access.csv',
+        'demo/demo.xml',
+    ],
+    # cargar datos de ejemplo
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
